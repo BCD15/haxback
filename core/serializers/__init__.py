4ef6ce5ca3e254df54ef5d940f8d3250026ec1fd
BCD15 <bruno69correadias@gmail.com>
2023-11-25 00:07:17 -0300

    All serializers

--- core/serializers/__init__.py
+++ core/serializers/__init__.py
@@ -1,1 +1,5 @@
 from .user import UserSerializer
+from .avaliador import AvaliadorSerializer
+from .empresa import EmpresaSerializer
+from.equipe import EquipeSerializer
+from .people import PeopleSerializer
